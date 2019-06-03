20bd6b6cda8f1a3d3906a343e39963e6047d0ebe
erudisch <erudisch@meetyoo.de>
2019-06-03 20:37:22 +0200

    add authorization service

---
+++ authorization-service-spring/src/main/java/eu/rudisch/authorizationserver/repository/UserDetailRepository.java
@@ -0,0 +1,13 @@
+package eu.rudisch.authorizationserver.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import eu.rudisch.authorizationserver.model.User;
+
+public interface UserDetailRepository extends JpaRepository<User, Integer> {
+
+	Optional<User> findByUsername(String name);
+
+}
