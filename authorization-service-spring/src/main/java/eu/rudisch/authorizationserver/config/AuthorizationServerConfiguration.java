06bf33962e8894b2fa5691efad8046106ea6ea30
Erik Rudisch <erik.rudisch@posteo.de>
2019-06-14 10:21:43 +0200

    add resource server

--- authorization-service-spring/src/main/java/eu/rudisch/authorizationserver/config/AuthorizationServerConfiguration.java
+++ authorization-service-spring/src/main/java/eu/rudisch/authorizationserver/config/AuthorizationServerConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
@@ -23,6 +24,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 @Configuration
+@EnableAuthorizationServer
 public class AuthorizationServerConfiguration implements AuthorizationServerConfigurer {
 
 	// https://www.baeldung.com/spring-security-oauth-jwt
@@ -38,7 +40,7 @@ public class AuthorizationServerConfiguration implements AuthorizationServerConf
 	private AuthenticationManager authenticationManager;
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Bean
 	public TokenStore tokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
