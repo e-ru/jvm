077ffe2e5f5fa0bb203041e8384bd5b844adb7a3
Erik Rudisch <erik.rudisch@posteo.de>
2019-06-21 00:53:34 +0200

    add wiremock

--- authorization-service-spring/src/test/java/eu/rudisch/authorizationserver/OAuthMvcTest.java
+++ authorization-service-spring/src/test/java/eu/rudisch/authorizationserver/OAuthMvcTest.java
@@ -1,5 +1,8 @@
 package eu.rudisch.authorizationserver;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.junit.Assert.fail;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -9,11 +12,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.ActiveProfiles;
@@ -22,11 +28,16 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultHandler;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
@@ -34,18 +45,27 @@ import org.springframework.web.context.WebApplicationContext;
 @ActiveProfiles("mvc")
 public class OAuthMvcTest {
 
-	public class SessionTracking implements ResultHandler {
-		private MockHttpSession lastSession;
+	@RestController
+	public class RedirectController {
 
-		@Override
-		public void handle(MvcResult result) throws Exception {
-			lastSession = (MockHttpSession) result.getRequest().getSession(false);
+		@RequestMapping(value = "/html", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+		@ResponseStatus(code = HttpStatus.OK)
+		public String getAccount() {
+
+			return "<!DOCTYPE html>\n" +
+					"<html>\n" +
+					"  <head>\n" +
+					"    <title>Auth-Server-Redirect</title>\n" +
+					"  </head>\n" +
+					"  <body>\n" +
+					"  </body>\n" +
+					"</html>";
 		}
 
-		public MockHttpSession getLastSession() {
-			return lastSession;
-		}
 	}
+
+	@Rule
+	public WireMockRule wireMockRule = new WireMockRule(9999);
 
 	@Autowired
 	private WebApplicationContext wac;
@@ -54,8 +74,6 @@ public class OAuthMvcTest {
 	private FilterChainProxy springSecurityFilterChain;
 
 	private MockMvc mockMvc;
-
-	private SessionTracking sessions;
 
 	private static final String CLIENT_ID = "auth_server";
 	private static final String CLIENT_SECRET = "secret";
@@ -67,12 +85,12 @@ public class OAuthMvcTest {
 
 	@Before
 	public void setup() {
-		sessions = new SessionTracking();
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).addFilter(springSecurityFilterChain)
 				.build();
 //		HtmlUnitDriver driver = MockMvcHtmlUnitDriverBuilder
 //			    .mockMvcSetup(mockMvc)
 //			    .build();
+//		MediaType.TEXT_HTML_VALUE
 	}
 
 	@Test
@@ -85,12 +103,37 @@ public class OAuthMvcTest {
 	}
 
 	private void obtainAuthorizationCode() throws Exception {
+		String htmlBody = "<!DOCTYPE html>\n" +
+				"<html>\n" +
+				"  <head>\n" +
+				"    <title>Auth-Server-Redirect</title>\n" +
+				"  </head>\n" +
+				"  <body>\n" +
+				"  </body>\n" +
+				"</html>";
+
+		stubFor(com.github.tomakehurst.wiremock.client.WireMock.get(
+				urlEqualTo("http://localhost:9999/redirect.html"))
+				.willReturn(aResponse()
+						.withStatus(200)
+						.withHeader("Content-Type", MediaType.TEXT_HTML_VALUE)
+						.withBody(htmlBody)));
+
+//		CloseableHttpClient httpClient = HttpClients.createDefault();
+//		HttpGet request = new HttpGet(String.format("http://localhost:9999/redirect.html"));
+//		HttpResponse httpResponse = httpClient.execute(request);
+//
+//		MvcResult mvcResult0 = mockMvc.perform(get("http://localhost:8889/redirect.html"))
+////				.andExpect(content().contentType(MediaType.TEXT_HTML_VALUE))
+//				.andExpect(status().isOk())
+//				.andReturn();
+
 		MockHttpSession session = new MockHttpSession();
 
 		String responseType = "code";
 		String scope = "create_oauth read_oauth update_oauth delete_oauth";
 		String clientId = "auth_server";
-		String redirectUrl = "http://localhost/redirect.html";
+		String redirectUrl = "http://localhost:8889/redirect.html";
 
 //		params.add("response_type", "code");
 //		params.add("scope", "create_oauth read_oauth update_oauth delete_oauth");
@@ -102,6 +145,10 @@ public class OAuthMvcTest {
 				+ "&scope=" + scope
 				+ "&client_id=" + clientId
 				+ "&redirect_uri=" + redirectUrl;
+
+//		CloseableHttpClient httpClient = HttpClients.createDefault();
+//		HttpGet request = new HttpGet(url);
+//		HttpResponse httpResponse = httpClient.execute(request);
 
 		MvcResult mvcResult = mockMvc.perform(get(url)
 //				.params(params)
@@ -132,9 +179,10 @@ public class OAuthMvcTest {
 
 		mvcResult = mockMvc.perform(post(url)
 				.session(session)
-				.params(params))
-				.andExpect(status().isFound())
-				.andExpect(redirectedUrl(redirectUrl))
+//				.params(params)
+		)
+//				.andExpect(status().isFound())
+//				.andExpect(redirectedUrl(redirectUrl))
 				.andReturn();
 
 		String resultString = mvcResult.getResponse().getRedirectedUrl();
