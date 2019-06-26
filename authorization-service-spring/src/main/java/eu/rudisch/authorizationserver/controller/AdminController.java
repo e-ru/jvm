aa8053f41b15bce8da9fbfa60be261f3ad2a66fe
Erik Rudisch <erik.rudisch@posteo.de>
2019-06-27 00:19:29 +0200

    add query by username

--- authorization-service-spring/src/main/java/eu/rudisch/authorizationserver/controller/AdminController.java
+++ authorization-service-spring/src/main/java/eu/rudisch/authorizationserver/controller/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +26,7 @@ public class AdminController {
 
 	@RequestMapping(value = "/users", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(value = HttpStatus.OK)
-	public @ResponseBody List<User> getUser() {
+	public @ResponseBody List<User> getUser(@RequestParam String username) {
 		List<User> users = userDetailRepository.findAll();
 		return users;
 	}
