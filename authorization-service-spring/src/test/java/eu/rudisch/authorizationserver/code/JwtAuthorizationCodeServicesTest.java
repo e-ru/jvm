e4601f22afac494d677c6269f09312a8af2f4e87
Erik Rudisch <erudisch@meetyoo.de>
2019-07-19 14:56:41 +0200

    separate auth code logic

--- authorization-service-spring/src/test/java/eu/rudisch/authorizationserver/code/JwtAuthorizationCodeServicesTest.java
+++ authorization-service-spring/src/test/java/eu/rudisch/authorizationserver/code/JwtAuthorizationCodeServicesTest.java
@@ -1,14 +1,16 @@
 package eu.rudisch.authorizationserver.code;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-import javax.sql.DataSource;
-
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -18,12 +20,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
+import org.springframework.security.oauth2.common.util.JsonParser;
+import org.springframework.security.oauth2.common.util.JsonParserFactory;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.OAuth2Request;
-import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -39,12 +41,22 @@ import eu.rudisch.authorizationserver.service.JwtExtractorImpl;
 @ActiveProfiles("test")
 class JwtAuthorizationCodeServicesTest {
 
+	static final String USERNAME = "test_user";
+	static final String ROLE = "test_role";
+	static final String PERMISSION = "test_permission";
+	static final String CLIENT_ID = "test_clientId";
+	static final Set<String> SCOPE = Set.of("test_scope");
+	static final Set<String> RESOURCE_IDS = Set.of("test_resource");
+	static final String REDIRECT_URI = "test_redirectUri";
+
+	static final String CODE = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0X2NsaWVudElkIiwicmRyIjoidGVzdF9yZWRpcmVjdFVyaSIsInVzciI6InRlc3RfdXNlciIsInBtcyI6WyJ0ZXN0X3Njb3BlIl0sImV4cCI6MzMxMjA0Nzc4NTI0MTcsImp0aSI6IjQ0NDdlNmE3LTI1NGItNDI0Yi05ZTlkLWViNThkODBjZmNmZiJ9.SEdC8UORHIqUjJ5ZSsK1tDL1KihNNR1lSlEkR3JSYseYa5udShEd2YPfJ_3nsR6qKioVDTM3ODapkC8TWPQl_dWLycS-zFKLXuIemHkrDAnzKlAVwnlTLBuIqHuGmKQcGyFfm4w266cXjmS1X2osCCIt_CHZ8yhwVxuxqzChBnfSTrbjGcgDwLnzKMK3cmEHm8l-ghAOSLrx-_0dWweObQD5XL__PGaKwJzVxVPUZIjaM2kM2ge4B-abdw26qJ0YJ_3bsS5RhAKYrDvbgyLKr-smTBUVKL7dsQxr4Jk-Rf122eRI6be5vEVs9peTMToDuUGJJnJKZ3LnrX9deBFoXw";
+
 	@TestConfiguration
 	static class JwtAuthorizationCodeServicesTestContextConfiguration {
-		@Bean
-		public DataSource dataSource() {
-			return Mockito.mock(DataSource.class);
-		}
+//		@Bean
+//		public DataSource dataSource() {
+//			return Mockito.mock(DataSource.class);
+//		}
 
 		@Bean
 		public JwtAuthorizationCodeServices jwtAuthorizationCodeServices() {
@@ -55,56 +67,85 @@ class JwtAuthorizationCodeServicesTest {
 	@Autowired
 	private JwtAuthorizationCodeServices jwtAuthorizationCodeServices;
 
-	@MockBean
-	private UserDetailsService userDetailsService;
-	@MockBean
-	private JdbcClientDetailsService jbcClientDetailsService;
-	@MockBean
-	private ClientDetails clientDetails;
-	@MockBean
-	private Authentication authentication;
-	@MockBean
-	private OAuth2Authentication oAuth2Authentication;
+//	@MockBean
+//	private UserDetailsService userDetailsService;
+//	@MockBean
+//	private JdbcClientDetailsService jbcClientDetailsService;
+//	@MockBean
+//	private ClientDetails clientDetails;
+//	@MockBean
+//	private Authentication authentication;
+//	@MockBean
+//	private OAuth2Authentication oAuth2Authentication;
 	@InjectMocks
 	private JwtExtractorImpl jwtExtractor;
-	@InjectMocks
+//	@InjectMocks
+	@MockBean
 	private JwtAuthorizationCodeServicesHelperServiceImpl helperService;
 
-	@Test
-	void createAuthorizationCode() throws InvalidKeySpecException, NoSuchAlgorithmException {
-		String userName = "test_user";
-		String role = "test_role";
-		String permission = "test_permission";
-		String clientId = "test_clientId";
-		Set<String> scope = Set.of("test_scope");
-		Set<String> resourceIds = Set.of("test_resource");
-		String redirectUri = "test_redirectUri";
+	private JsonParser objectMapper = JsonParserFactory.create();
 
+	private static OAuth2Authentication oAuth2Authentication;
+
+	@BeforeAll
+	public static void init() {
 		Permission p = new Permission();
 		Role r = new Role();
 		User u = new User();
-		p.setName(permission);
-		r.setName(role);
+		p.setName(PERMISSION);
+		r.setName(ROLE);
 		r.setPermissions(List.of(p));
-		u.setUsername(userName);
+		u.setUsername(USERNAME);
 		u.setRoles(List.of(r));
-		OAuth2Request oAuth2Request = new OAuth2Request(null, clientId, null, true, scope, resourceIds, redirectUri,
-				null, null);
 		AuthUserDetail authUserDetail = new AuthUserDetail(u);
 
-		Mockito.when(oAuth2Authentication.getUserAuthentication()).thenReturn(authentication);
-		Mockito.when(authentication.getPrincipal()).thenReturn(u);
-		Mockito.when(oAuth2Authentication.getOAuth2Request()).thenReturn(oAuth2Request);
+		oAuth2Authentication = new OAuth2Authentication(
+				new OAuth2Request(null, CLIENT_ID, null, true, SCOPE, RESOURCE_IDS, REDIRECT_URI,
+						null, null),
+				new CodeAuthentication(authUserDetail));
+	}
 
-		Mockito.when(userDetailsService.loadUserByUsername(userName)).thenReturn(authUserDetail);
-		Mockito.when(jbcClientDetailsService.loadClientByClientId(clientId)).thenReturn(clientDetails);
-		Mockito.when(clientDetails.getResourceIds()).thenReturn(resourceIds);
-
+	@Test
+	void createAuthorizationCode() throws InvalidKeySpecException, NoSuchAlgorithmException {
 		String code = jwtAuthorizationCodeServices.createAuthorizationCode(oAuth2Authentication);
 
-		jwtAuthorizationCodeServices.consumeAuthorizationCode(code);
+		Jwt jwtCode = JwtHelper.decode(code);
+		String claimsStr = jwtCode.getClaims();
+		Map<String, Object> claims = objectMapper.parseMap(claimsStr);
 
-		assertNotNull(jwtAuthorizationCodeServices);
+		assertEquals(claims.get(JwtAuthorizationCodeServices.SUBJECT), CLIENT_ID);
+		assertEquals(claims.get(JwtAuthorizationCodeServices.CLAIM_REDIRECT_URL), REDIRECT_URI);
+		assertEquals(claims.get(JwtAuthorizationCodeServices.CLAIM_USER), USERNAME);
+		assertEquals(jwtAuthorizationCodeServices.scope(claims.get(JwtAuthorizationCodeServices.CLAIM_PERMISSIONS)),
+				SCOPE);
+	}
+
+	@Test
+	public void consumeAuthorizationCode() {
+		Jwt jwtCode = JwtHelper.decode(CODE);
+		String claimsStr = jwtCode.getClaims();
+		Map<String, Object> claims = objectMapper.parseMap(claimsStr);
+
+		String clientId = (String) claims.get(JwtAuthorizationCodeServices.SUBJECT);
+		Set<String> scope = jwtAuthorizationCodeServices
+				.scope(claims.get(JwtAuthorizationCodeServices.CLAIM_PERMISSIONS));
+		String redirectUrl = (String) claims.get(JwtAuthorizationCodeServices.CLAIM_REDIRECT_URL);
+		String user = (String) claims.get(JwtAuthorizationCodeServices.CLAIM_USER);
+
+		Mockito.when(helperService.oAuth2Authentication(clientId, scope, redirectUrl, user))
+				.thenReturn(oAuth2Authentication);
+
+		OAuth2Authentication oAuth2Authentication = jwtAuthorizationCodeServices.consumeAuthorizationCode(CODE);
+
+		Set<String> authorities = oAuth2Authentication.getAuthorities().stream()
+				.map(ga -> ga.getAuthority())
+				.collect(Collectors.toSet());
+		assertTrue(authorities.contains(ROLE));
+		assertTrue(authorities.contains(PERMISSION));
+		assertEquals(oAuth2Authentication.getOAuth2Request().getClientId(), CLIENT_ID);
+		assertEquals(oAuth2Authentication.getOAuth2Request().getRedirectUri(), REDIRECT_URI);
+		assertEquals(oAuth2Authentication.getOAuth2Request().getResourceIds(), RESOURCE_IDS);
+		assertEquals(oAuth2Authentication.getUserAuthentication().getName(), USERNAME);
 	}
 
 }
