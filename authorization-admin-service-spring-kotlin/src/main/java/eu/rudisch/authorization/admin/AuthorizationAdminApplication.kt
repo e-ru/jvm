f1c5038c497e607915d01c87cfcfe95a48936f81
Erik Rudisch <erudisch@meetyoo.de>
2019-08-06 18:32:31 +0200

    add admin kotlin

---
+++ authorization-admin-service-spring-kotlin/src/main/java/eu/rudisch/authorization/admin/AuthorizationAdminApplication.kt
@@ -0,0 +1,11 @@
+package eu.rudisch.authorization.admin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthorizationAdminApplication {
+	fun main(args: Array<String>) {
+		runApplication<AuthorizationAdminApplication>(*args)
+	}
+}
---
+++ authorization-admin-service-spring-kotlin/src/main/java/eu/rudisch/authorization/admin/config/AuthorizationAdminConfig.kt
@@ -0,0 +1,10 @@
+package eu.rudisch.authorization.admin.config
+
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter
+
+class AuthorizationAdminConfig : ResourceServerConfigurerAdapter() {
+companion object {
+    val info = "This is info"    
+    fun getMoreInfo():String { return "This is more fun" }
+  }
+}
