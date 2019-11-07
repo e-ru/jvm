e82fd7354c8febe95d12875fd9f5c5bee25ec610
e-ru <e.rudisch@gmx.de>
2019-11-07 01:08:58 +0100

    add restful endpoints

--- long-polling/src/main/java/eu/rudisch/sse/EventController.kt
+++ long-polling/src/main/java/eu/rudisch/sse/EventController.kt
@@ -2,12 +2,11 @@ package eu.rudisch.sse
 
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.http.codec.ServerSentEvent
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 import java.text.SimpleDateFormat
 import java.time.Duration
@@ -16,37 +15,33 @@ import java.util.*
 import java.util.function.Consumer
 
 @RestController
-class Controller(
+@RequestMapping("/events")
+class EventController(
 		@Qualifier("stringProcessor")
 		private val stringProcessor: Processor<String>,
 		@Qualifier("sseProcessor")
 		private val sseProcessor: Processor<ServerSentEvent<String>>) {
 
 	companion object {
-		private val logger = LoggerFactory.getLogger(Controller::class.java)
+		private val logger = LoggerFactory.getLogger(EventController::class.java)
 	}
 
-	@PostMapping("/send")
-	fun send(@RequestBody message: String): String {
-		logger.info("Received '{}'", message)
-		stringProcessor.process(SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date()) + " " + message)
+	@PostMapping()
+	@ResponseStatus(value = HttpStatus.CREATED)
+	fun send(@RequestBody event: String) {
+		logger.info("Received '{}'", event)
+		stringProcessor.process(SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date()) + " " + event)
 		sseProcessor.process(
 				ServerSentEvent.builder<String>()
-						.id(message)
+						.id(event)
 						.event("periodic-event")
 						.data("SSE - " + LocalTime.now().toString())
 						.build()
 		)
-		return "Done"
 	}
 
-	@GetMapping(path = ["/receive-sse"])
+	@GetMapping()
 	fun receiveSSE(): Flux<ServerSentEvent<String>> {
-		// Some FluxSink documentation and code samples:
-		// - https://projectreactor.io/docs/core/release/reference/#producing.create
-		// - https://www.baeldung.com/reactor-core
-		// - https://www.e4developer.com/2018/04/14/webflux-and-servicing-client-requests-how-does-it-work/
-
 		return Flux.create { sink ->
 			sink?.let {
 				sseProcessor.register(Consumer { sink.next(it) })
@@ -54,7 +49,7 @@ class Controller(
 		}
 	}
 
-	@GetMapping(path = ["/receive-flux"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+	@GetMapping(path = ["/flux"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
 	fun receiveFlux(): Flux<String> {
 		// Some FluxSink documentation and code samples:
 		// - https://projectreactor.io/docs/core/release/reference/#producing.create
